4b4ec45aa258779645ae69d41f47515e11122e45
Steve Townend <steve@townendmail.co.uk>
2017-03-10 22:43:31 +0000

    Need to fix timestamp bug

--- gitIntroTest/Program.cs
+++ gitIntroTest/Program.cs
@@ -18,6 +18,12 @@ namespace gitIntroTest
 
             Console.WriteLine("My third bit of code");
 
+            Console.WriteLine("Lots 1");
+            Console.WriteLine("Lots 2");
+            Console.WriteLine("Lots 3");
+            Console.WriteLine("Lots 4");
+            Console.WriteLine("Lots 5");
+
             Console.Read();
         }
 
