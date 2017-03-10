99e5a64b9f1a19643986f92a0d4f059a103891ee
Steve Townend <steve@townendmail.co.uk>
2017-03-10 20:55:42 +0000

    First commit

---
+++ gitIntroTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitIntroTest
+{
+    class Program
+    {
+        private const string VERSION = "1.0";
+
+        static void Main(string[] args)
+        {
+            showVersion();
+            Console.WriteLine("Very first bit of code");
+
+            Console.Read();
+        }
+
+        private static void showVersion()
+        {
+            Console.WriteLine("Version : " + VERSION);
+        }
+    }
+}
